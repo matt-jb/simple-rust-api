5ac366de6c6a0b934c5b7f140f7952106b9c5068
Mateusz Jarzębowski-Bownik <mateusz.jarzebowski.bownik@gmail.com>
2022-08-31 13:32:07 +0200

    Added logging

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,16 @@
-use actix_web::{App, HttpServer};
+use actix_web::{App, HttpServer, middleware::Logger};
 mod api;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    std::env::set_var("RUST_LOG", "info");
+    std::env::set_var("RUST_BACKTRACE", "1");
+    env_logger::init();
+
     HttpServer::new(|| {
+        let logger = Logger::default();
         App::new()
+            .wrap(logger)
             .service(api::get_fuel_usage)
             .service(api::get_fail_probability)
     })
